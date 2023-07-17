15255abe5692e711b73434704bafbec4b8be8e6e
Sam <sam.camhi@gmail.com>
2023-07-16 23:37:33 -0400

    Passed Tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+
+    if length == 0:
+       print([])
+    elif length == 1:
+       print([0])
+    else:    
+        fibo = [0]*length
+        fibo[0] = 0
+        fibo[1] = 1
+
+        for i in range(2,length):
+            fibo[i] = fibo[i-1] + fibo[i-2]
+
+        print(fibo)
+
+
+
+
     pass
